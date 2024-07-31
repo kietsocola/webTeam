6376066749bca18e2ca5e5592ba040ee7ec2e3f3
kietsocola <vankiet27012004@gmail.com>
2024-07-31 23:49:15 +0700

    add AJAX changing page, but has some error change status task

--- src/main/java/com/myProject/webteam/controllers/ChangingPage.java
+++ src/main/java/com/myProject/webteam/controllers/ChangingPage.java
@@ -11,45 +11,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.myProject.webteam.dto.TaskDTO;
 import com.myProject.webteam.models.Project;
-import com.myProject.webteam.models.Task;
 import com.myProject.webteam.services.CategoryService;
 import com.myProject.webteam.services.ProjectService;
-import com.myProject.webteam.services.TaskService;
 
 @Controller
-public class ProjectController {
+public class ChangingPage {
 	private ProjectService projectService;
 	private CategoryService categoryService;
 	@Autowired
-	public ProjectController(ProjectService projectService, CategoryService categoryService) {
+	public ChangingPage(ProjectService projectService, CategoryService categoryService) {
 		this.projectService = projectService;
 		this.categoryService = categoryService;
 	}
-	@GetMapping("/project")
-	public String home(Model model) {
-		List<Project> projects = projectService.getListProject();
-		model.addAttribute("projects", projects);
-		Project project = projects.getLast();
-		model.addAttribute("projectFindById", project);
-		return "board/index";
-	}
-	@PostMapping("/addProject")
-	public String addProject(Model model, @RequestParam String nameProject) {
-		projectService.saveProject(nameProject);
-		List<Project> projects = projectService.getListProject();
-		model.addAttribute("projects", projects);
-		Project project = projects.getLast();
-		model.addAttribute("projectFindById", project);
-		return "board/index";
-	}
-	@GetMapping("/project/changePage")
-	public String detailProject(Model model, @RequestParam("idProject") int idProject) {
+	@GetMapping("/summary/{section}")
+    public String getSummary(@PathVariable String section) {
+        return "summary/" + section;
+    }
+	@GetMapping("/board/{section}")
+    public String getSection(@PathVariable String section, Model model, @RequestParam int idProject) {
 		List<Project> projects = projectService.getListProject();
 		model.addAttribute("projects", projects);
 		Project project = projectService.getProjectById(idProject);
@@ -67,6 +51,6 @@ public class ProjectController {
 		List<Integer> loopList = Arrays.asList(1, 2, 3);
         model.addAttribute("loopList", loopList);
         model.addAttribute("taskNew", new TaskDTO());
-		return "project/index";
-	}
+        return "board/" + section;
+    }
 }
